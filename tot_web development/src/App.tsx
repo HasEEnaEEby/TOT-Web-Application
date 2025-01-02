c50d8b364f793395b3dcb224ab577cf19ff3f214
Haseena <121224804+HasEEnaEEby@users.noreply.github.com>
2025-01-02 13:39:06 +0545

    C1: 3d obj has been added in the for more interaction

--- tot_web development/src/App.tsx
+++ tot_web development/src/App.tsx
@@ -1,44 +1,19 @@
-import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes'; 
-import { ThemeToggle } from './components/common/ThemeToggle'; 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const [isDark, setIsDark] = useState(() => {
-    if (typeof window !== 'undefined') {
-      return localStorage.getItem('theme') === 'dark';
-    }
-    return false;
-  });
-
-  const toggleTheme = () => {
-    setIsDark((prev) => {
-      const newTheme = !prev;
-      localStorage.setItem('theme', newTheme ? 'dark' : 'light');
-      document.documentElement.classList.toggle('dark', newTheme);
-      return newTheme;
-    });
-  };
-
-  React.useEffect(() => {
-    document.documentElement.classList.toggle('dark', isDark);
-  }, [isDark]);
-
   return (
     <Router>
-      <ThemeToggle isDark={isDark} toggleTheme={toggleTheme} />
-      <>
-        {/* App routes */}
-        <AppRoutes />
-        
-        {/* Toast notifications */}
-        <ToastContainer />
-      </>
+    
+      {/* App routes */}
+      <AppRoutes />
+      
+      {/* Toast notifications */}
+      <ToastContainer />
     </Router>
   );
 };
-
 
 export default App;
